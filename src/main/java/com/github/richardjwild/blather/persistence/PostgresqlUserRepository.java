831c6af2e7a05896cb4a4cd137243a2d83d81905
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-04-15 17:11:19 +0100

    Add tests for PostgresqlUserRepository

--- src/main/java/com/github/richardjwild/blather/persistence/PostgresqlUserRepository.java
+++ src/main/java/com/github/richardjwild/blather/persistence/PostgresqlUserRepository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 public class PostgresqlUserRepository implements UserRepository {
     private PostgresConnector postgresDbConnector;
 
-    public PostgresqlUserRepository(PostgresConnector postgresDbConnector) {
+    public PostgresqlUserRepository() {
         this.postgresDbConnector = postgresDbConnector;
     }
 
--- src/test/java/com/github/richardjwild/blather/persistence/PostgresqlUserRepositoryShould.java
+++ src/test/java/com/github/richardjwild/blather/persistence/PostgresqlUserRepositoryShould.java
@@ -2,20 +2,68 @@ package com.github.richardjwild.blather.persistence;
 
 import com.github.richardjwild.blather.user.User;
 import com.github.richardjwild.blather.user.UserRepository;
+import junitparams.naming.TestCaseName;
+import org.flywaydb.core.Flyway;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import java.util.Optional;
+
+import static org.fest.assertions.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
 
+@TestCaseName("Fragile")
 public class PostgresqlUserRepositoryShould {
+    String testDatabaseUrl = "jdbc:postgresql://localhost:5432/test";
+    String testUser = "testuser";
+    String password = "testPassword123";
+    Flyway flyway = Flyway.configure().dataSource(testDatabaseUrl, testUser, password).load();
+
+    private UserRepository userRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        flyway.migrate();
+        userRepository = new PostgresqlUserRepository();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        flyway.clean();
+    }
+
     @Test
-    public void adds_a_new_user_to_the_persistent_store() {
-        PostgresConnector postgresDbConnector = Mockito.mock(PostgresConnector.class);
-        UserRepository postgresUserRepository = new PostgresqlUserRepository(postgresDbConnector);
-        User user = new User("user_name");
+    public void return_empty_when_user_not_found() {
+        Optional<User> result = userRepository.find("will_not_be_found");
 
-        postgresUserRepository.save(user);
+        assertThat(result.isPresent()).isFalse();
+    }
 
-        verify(postgresDbConnector).add_user("user_name");
+    @Test
+    public void return_stored_user_when_user_is_found() {
+        String userName = "will_be_found";
+        User expectedUser = new User(userName);
+        userRepository.save(expectedUser);
+
+        Optional<User> actualUser = userRepository.find(userName);
+
+        assertThat(actualUser.isPresent()).isTrue();
+        assertThat(actualUser.get()).isSameAs(expectedUser);
+    }
+
+    @Test
+    public void not_store_duplicate_users_when_the_same_user_saved_twice() {
+        String userName = "user_name";
+        User user = new User(userName);
+        userRepository.save(user);
+        User userWithSameName = new User(userName);
+
+        userRepository.save(userWithSameName);
+
+        Optional<User> actualUser = userRepository.find(userName);
+        assertThat(actualUser.get()).isSameAs(user);
+        assertThat(actualUser.get()).isNotSameAs(userWithSameName);
     }
 }
